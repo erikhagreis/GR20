db448dd9282a0f53e874d865f0e2d1d94c382c65
Erik Hagreis <erik.hagreis@gmail.com>
2018-04-27 11:23:03 +0200

    feat: initial scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const { version, name, author} = require('./package.json');
+
+module.exports = {
+  mode: 'development',
+  entry: [ './src/polyfills.ts', './src/main.ts' ],
+  output: {
+    path: path.join(__dirname, './dist/'),
+    filename: 'gr20.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: 'awesome-typescript-loader'
+      }, {
+        test: /\.js$/,
+        use: 'source-map-loader',
+        enforce: 'pre'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin(`${name} v${version} - (c) 2018 ${author}`)
+  ],
+  stats: { colors: true },
+  devtool: 'source-map'
+};
