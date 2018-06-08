f3da9245f449868dbb9aa8f279be353e18245800
Erik Hagreis <erik.hagreis@gmail.com>
2018-06-08 08:37:37 +0200

    perf(elevations): vastly improve getElevations performance

--- scripts/dataprep/elevation/getElevations.js
+++ scripts/dataprep/elevation/getElevations.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
-const { compose, find, isEqual, map, take, uniqBy } = require('lodash/fp');
+const { find, uniqBy } = require('lodash/fp');
 const config = require('../config');
 
 const load = (fileName) => fs.readJsonSync(path.join(config.INPUT_DIR, 'elevations', fileName), 'utf-8');
@@ -12,7 +12,10 @@ const save = (fileName, data) => {
   });
 };
 
-const findCoords3D = (known, coords2D) => find(compose(isEqual(coords2D), take(2)), known);
+const coordsMatch = (coords2D) => (coords3D) => 
+  (coords2D[0] === coords3D[0] && coords2D[1] === coords3D[1]);
+
+const findCoords3D = (known, coords2D) => find(coordsMatch(coords2D), known);
 
 module.exports = function init() {
   let known = load('known.json');
@@ -24,19 +27,22 @@ module.exports = function init() {
     for (const coords2D of coordsList2D) {
       const coords3D = findCoords3D(known, coords2D);
       results = [ ...results, coords3D || coords2D ];
-
+      
       if (!coords3D) {
         missing = [ ...missing, coords2D ];
       }
     }
-
     requested = [ ...requested, ...missing ];
-
+    
+    console.log(
+      `getElevations retrieving ${coordsList2D.length} coordinates from a known list of ${known.length}. `
+    + `Missing and added to requested list: ${missing.length}.`);
     return results;
   };
 
   const saveRequestedElevations = () => {
     requested = uniqBy(([lng, lat]) => `${lng},${lat}`, requested);
+    console.log(`saveRequestedElevations saving a list of ${requested.length} coordinates to query.`);
     save('requested.json', requested);
   };
 
